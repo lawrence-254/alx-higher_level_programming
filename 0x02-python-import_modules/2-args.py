9240cbd0d97dac37cd88bfdfe27543f7b11bc669
lawrence-254 <lodhiambo26@gmail.com>
2023-06-08 03:09:59 -0700

    4

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("1 argument:")
+    elif count == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
