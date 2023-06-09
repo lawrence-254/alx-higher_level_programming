6caccd9a03f38f63bb120e987ec2c334118de6f8
lawrence-254 <lodhiambo26@gmail.com>
2023-06-09 02:59:52 -0700

    0

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    prod = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            prod.append(True)
+        else:
+            prod.append(False)
+
+        return (prod)
