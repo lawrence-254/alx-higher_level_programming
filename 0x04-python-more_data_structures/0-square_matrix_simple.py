aa3c7d15f81a42e88d5ea45e8507150bd6f5ad54
lawrence-254 <lodhiambo26@gmail.com>
2023-06-14 10:44:18 +0000

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	matrix_1 = matrix.copy()
+    matrix_1 = matrix.copy()
 
-	for i in range(len(matrix)):
-		matrix_1 = list(map(lambda x: x**2, matrix[i]))
-	return(matrix_1)
+    for i in range(len(matrix)):
+        matrix_1[i] = [x**2 for x in matrix[i]]
+
+    return (matrix_1)
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+	u_list = set(my_list)
+	list_n = 0
+
+	for i in u_list:
+		list_n =+ i
+	return list_n
