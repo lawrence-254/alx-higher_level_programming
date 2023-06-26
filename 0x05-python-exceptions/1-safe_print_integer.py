60d877473a98479223621b5f61e08d6be3d2e792
lawrence-254 <lodhiambo26@gmail.com>
2023-06-26 16:08:48 +0000

    1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""a function that prints an integer with "{:d}".format()"""
+"""
+a function that prints an integer with "{:d}".format()
+Returns the real number of integers printed
+"""
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
