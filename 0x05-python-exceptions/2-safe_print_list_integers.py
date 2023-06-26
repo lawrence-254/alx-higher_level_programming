0378ba77245999654ed1ba60bab2a006c0edee32
lawrence-254 <lodhiambo26@gmail.com>
2023-06-26 16:31:48 +0000

    2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+	''' a function that prints the first x elements of a list and only integers'''
+	count = 0
+	try:
+		for i in range(x):
+			if isinstance(my_list[i], int):
+				print("{:d}".format(my_list[i]), end='')
+				count += 1
+	except (IndexError, TypeError):
+		pass
+	finally:
+		print('')
+		return (count)
