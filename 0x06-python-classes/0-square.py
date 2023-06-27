1e8e54765f1309797aa3f4c3715280b7a9a97281
lawrence-254 <lodhiambo26@gmail.com>
2023-06-27 09:22:33 +0000

    0+1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 class Square:
-	"""Empty class"""
-	"""this class does nothing hence"""
-	pass
+ """Empty class"""
+ """this class does nothing hence"""
+ pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
-	"""A square that defines a square"""
-	def __init__(self, size):
-		"""Declares private property sqare"""
-		self.__size = size
+ """A square that defines a square"""
+ def __init__(self, size):
+  """Declares private property sqare"""
+  self.__size = size
 
