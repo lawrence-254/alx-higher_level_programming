a0a6e4b118fc7bdb877383d6b83b4b401e4833b5
lawrence-254 <lodhiambo26@gmail.com>
2023-07-03 11:57:55 -0700

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """Defines a Rectangle with dimensions."""
 
@@ -6,23 +7,23 @@ class Rectangle:
     """A rectangle class."""
 
     def __init__(self, width=0, height=0):
-        """Rectangle initialisation with dimension defined
+        """Rectangle initialisation with dimension defined.
 
         Args:
-            width(int):width is the shortest side measurements of the rectacle
-            height(int):the mesurement of the longer side
+            width(int):width is the shortest side measurements of the rectacle.
+            height(int):the mesurement of the longer side.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """A get function for the width of the rectangle"""
+        """A get function for the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """A set function for the width property that ensures it is an int"""
+        """A set function for the width property that ensures it is an int."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -31,13 +32,13 @@ class Rectangle:
 
     @property
     def height(self):
-        """A get function for the width of the rectangle"""
+        """A get function for the width of the rectangle."""
         return self.__height
 
 
     @height.setter
     def height(self, value):
-        """Set method for the height property"""
+        """Set method for the height property."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
