31d3a03cca9d246fab329e56a9de6291a8ca32cb
lawrence-254 <lodhiambo26@gmail.com>
2023-07-11 11:27:01 -0700

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class object student."""
+
+
+class Student:
+    """Class object student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes student.
+
+        Args:
+            first_name (str)
+            last_name (str)
+            age (int)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get sudent dictionary.
+
+        Args:
+            attrs (list)
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
