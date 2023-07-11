13f24b0bedc790476865ec7303e2ccd9ae09497d
lawrence-254 <lodhiambo26@gmail.com>
2023-07-11 09:47:41 -0700

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that reads a text"""
+
+
 def read_file(filename=""):
     """Print the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
