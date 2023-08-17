3ba704ce45fa8aefefe3553df88076719f8f92e1
lawrence-254 <lodhiambo26@gmail.com>
2023-08-17 02:32:41 -0700

    11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in the database
+
+USE hbtn_0d_tvshows;
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
