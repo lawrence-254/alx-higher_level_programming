d6e4dda398bd96d32412c2a3aa734e2d2c74a617
lawrence-254 <lodhiambo26@gmail.com>
2023-09-11 12:44:23 -0700

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 const args = process.argv[2];
 
 if (args !== undefined) {
-	console.log(args);
+  console.log(args);
 } else {
-	console.log('No arguments');
+  console.log('No arguments');
 }
