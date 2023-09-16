dbed958a8901ce760cb59d7ce2db9a4fe554beed
lawrence-254 <lodhiambo26@gmail.com>
2023-09-16 08:10:03 -0700

    1re

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module that contains the City class definition.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    City class that inherits from Base
+    """
+    __tablename__ = 'cities'
+
+    id = Column(
+            Integer,
+            primary_key=True,
+            nullable=False,
+            unique=True,
+            autoincrement=True
+            )
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
