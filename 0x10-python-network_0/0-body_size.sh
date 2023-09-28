119f56709ad63a65d5ae3f19e53e488b8f415582
lawrence-254 <lodhiambo26@gmail.com>
2023-09-28 04:36:59 -0700

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL,
-#and displays the size of the body of the response
-
+# a Bash script that takes in a URL, sends a request to that URL
 curl -s -o "$1" | wc -c
-
