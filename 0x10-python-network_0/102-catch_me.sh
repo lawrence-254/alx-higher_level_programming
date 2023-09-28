f2d0ac4740d4cad0fd7800b63ebac5e51197dc0c
lawrence-254 <lodhiambo26@gmail.com>
2023-09-28 07:17:26 -0700

    102 catch me

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# causes the server to respond with a message containing You got me
+curl -s -X PUT -H "Expect: 100-continue" "http://0.0.0.0:5000/catch_me" -d "user_id=98" >/dev/null 2>&1
