4c50a980be7ee693c8b8f5f206ae08c688f1ff02
lawrence-254 <lodhiambo26@gmail.com>
2023-09-29 05:23:30 -0700

    0 corrected

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(url) as result:
             content = result.read()
-            print("Body response:$")
-            print(f"\t- type: {type(content)}")
-            print(f"\t- content: {content}")
-            print(f"\t- utf8 content: {content.decode('utf-8')}")
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
     except Exception:
         pass
