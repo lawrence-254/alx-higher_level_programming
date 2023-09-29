2705a2b63208a026021147bfa757560b0e3db0bc
lawrence-254 <lodhiambo26@gmail.com>
2023-09-29 02:45:46 -0700

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,9 @@
 import urllib.request
 
 
-try:
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as result:
-        content = result.read()
-        print("Body response:$")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
-except Exception:
-    pass
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as result:
+    content = result.read()
+    print("Body response:$")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
