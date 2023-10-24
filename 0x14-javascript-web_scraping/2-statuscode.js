1ce2b2a34d84e9900bbc8ecf57dacd07f5ec8d3b
lawrence-254 <lodhiambo262gmail.com>
2023-10-24 07:41:24 -0700

    2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request.
+
+
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log('code:', response.statusCode);
+});
