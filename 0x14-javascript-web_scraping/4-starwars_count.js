25bd881afb03a64acf808cab0e54a647311f3baf
lawrence-254 <lodhiambo262gmail.com>
2023-10-24 13:27:11 -0700

    4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+//a script that prints the number of movies
+//where the character “Wedge Antilles” is present.
+
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+let count = 0;
+
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const movies = JSON.parse(body).results;
+  for (const movie of movies) {
+    for (const character of movie.characters) {
+      if (character.includes('18')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
