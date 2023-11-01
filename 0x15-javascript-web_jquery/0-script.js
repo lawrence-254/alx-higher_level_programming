ba33e5999f6e84204294972952a7bb5ecdc07820
lawrence-254 <lodhiambo262gmail.com>
2023-11-01 02:10:01 -0700

    0

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//JavaScript script that updates the text color
+//of the <header> element to red (#FF0000)
+
+
+function updateHeaderColor(){
+	const headerElement = document.queryselector('heade');
+
+	headerElement.style.color = '#FF0000';
+}
